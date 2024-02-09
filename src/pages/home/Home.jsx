e1817786a43aeaa40dd7e6df68702a8b5fb50147
olofindamilola <kdammy2017@gmail.com>
2024-02-09 13:27:58 +0100

    home done

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,19 @@
+import Navbarr from "../../components/navbar/Navbarr";
+import Sidebar from "../../components/sidebar/Sidebar"
+import "./home.scss";
+
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbarr />
+        container
+        </div>
+        
+    </div>
+  )
+}
+
+export default Home
