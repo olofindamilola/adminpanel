d1c125ced3ff051f4042da33dd4c34c7654e673c
olofindamilola <kdammy2017@gmail.com>
2024-02-15 16:07:49 +0100

    admin dashboard done

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,69 +1,146 @@
 import "./widget.scss";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
-
+import { useEffect, useState } from "react";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
 
 const Widget = ({ type }) => {
-    let data;
-const amount = 100;
-const diff = 20;
-
+    const [amount, setAmount] = useState(null);
+    const [diff, setDiff] = useState(null);
+  
+    let data = {
+      title: "Default Title",
+      isMoney: false,
+      link: "Default Link",
+      icon: <span>Default Icon</span>,
+      query: null, // Initialize query as null
+    };
+  
     switch (type) {
-        case "user":
+      case "user":
         data = {
-            title: "USERS",
-            isMoney: false,
-            link: "see all users",
-            icon: <PersonOutlinedIcon  className="icon" style={{color: "crimson", backgroundColor: "rgba(255, 0, 0, 0.2)", }}/>,
-        }
+          ...data,
+          title: "USERS",
+          isMoney: false,
+          link: "See all users",
+          query: "users",
+          icon: (
+            <PersonOutlinedIcon
+              className="icon"
+              style={{
+                color: "crimson",
+                backgroundColor: "rgba(255, 0, 0, 0.2)",
+              }}
+            />
+          ),
+        };
         break;
-        case "order":
+      case "order":
         data = {
-            title: "ORDERS",
-            isMoney: false,
-            link: "view all orders",
-            icon: <ShoppingCartOutlinedIcon className="icon" style={{backgroundColor: "rgba(218, 165, 32, 0.2)", color: "goldenrod"}} />
-        }
+          ...data,
+          title: "ORDERS",
+          isMoney: false,
+          link: "View all orders",
+          icon: (
+            <ShoppingCartOutlinedIcon
+              className="icon"
+              style={{
+                backgroundColor: "rgba(218, 165, 32, 0.2)",
+                color: "goldenrod",
+              }}
+            />
+          ),
+        };
         break;
-        case "earning":
+      case "earning":
         data = {
-            title: "EARNINGS",
-            isMoney: true,
-            link: "view net earnings",
-            icon: <MonetizationOnOutlinedIcon  className="icon" style={{ backgroundColor: "rgba(0, 128, 0, 0.2)"}}/>,
-        }
+          ...data,
+          title: "EARNINGS",
+          isMoney: true,
+          link: "View net earnings",
+          icon: (
+            <MonetizationOnOutlinedIcon
+              className="icon"
+              style={{ backgroundColor: "rgba(0, 128, 0, 0.2)", color: "green" }}
+            />
+          ),
+        };
         break;
-        case "balance":
+      case "product":
         data = {
-            title: "BALANCE",
-            isMoney: true,
-            link: "see details",
-            icon: <AccountBalanceWalletOutlinedIcon  className="icon" style={{backgroundColor: "rgba(128, 0, 128, 0.2)"}}/>,
-        }
+          ...data,
+          title: "PRODUCTS",
+          query: "products",
+          link: "See details",
+          icon: (
+            <AccountBalanceWalletOutlinedIcon
+              className="icon"
+              style={{
+                backgroundColor: "rgba(128, 0, 128, 0.2)",
+                color: "purple",
+              }}
+            />
+          ),
+        };
         break;
-        default:
-            break;
+      default:
+        break;
     }
-  return (
-    <div className="widget">
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        if (data.query) {
+          const today = new Date();
+          const lastMonth = new Date(new Date().setMonth(today.getMonth() - 1));
+          const prevMonth = new Date(new Date().setMonth(today.getMonth() - 2));
+  
+          const lastMonthQuery = query(
+            collection(db, data.query),
+            where("timeStamp", "<=", today),
+            where("timeStamp", ">", lastMonth)
+          );
+          const prevMonthQuery = query(
+            collection(db, data.query),
+            where("timeStamp", "<=", lastMonth),
+            where("timeStamp", ">", prevMonth)
+          );
+  
+          const lastMonthData = await getDocs(lastMonthQuery);
+          const prevMonthData = await getDocs(prevMonthQuery);
+  
+          setAmount(lastMonthData.docs.length);
+          setDiff(
+            ((lastMonthData.docs.length - prevMonthData.docs.length) / prevMonthData.docs.length) *
+              100
+          );
+        }
+      };
+      fetchData();
+    }, [data.query]);
+  
+    return (
+      <div className="widget">
         <div className="left">
-            <span className="title">{data.title}</span>
-            <span className="counter">{data.isMoney && "$"}{amount}</span>
-            <span className="link">{data.link}</span>
+          <span className="title">{data.title}</span>
+          <span className="counter">
+            {data.isMoney && "$"} {amount}
+          </span>
+          <span className="link">{data.link}</span>
         </div>
         <div className="right">
-            <div className="percentage positive">
-                <KeyboardArrowUpIcon />
-                {diff}%
-            </div>
-                <PersonOutlinedIcon  className="icon"/>
-                {data.icon}
+          <div className={`percentage ${diff < 0 ? "negative" : "positive"}`}>
+            {diff < 0 ? <KeyboardArrowDownIcon /> : <KeyboardArrowUpIcon />}
+            {diff} %
+          </div>
+          {data.icon}
         </div>
-    </div>
-  )
-}
-
-export default Widget
+      </div>
+    );
+  };
+  
+  export default Widget;
